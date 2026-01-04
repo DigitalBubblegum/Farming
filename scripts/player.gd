f3c9ff2b4c65582f03ee33930f5edee577670ce2
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2026-01-04 17:37:17 +0530

    added enums for tools

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,21 @@
 extends CharacterBody2D
 var direction: Vector2
-var speed: int = 10000
+var speed: int = 500
 @onready var move_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/MoveStateMachine/playback")
-func _physics_process(delta: float) -> void:
+enum Tools {HOE, AXE, WATERINGCAN}
+var current_tool: Tools = Tools.AXE
+
+func _physics_process(_delta: float) -> void:
 	get_input()
 	animation()
-	velocity = direction * speed * delta
+	velocity = direction * speed
 	move_and_slide()
-	
+
 func get_input():
 	direction = Input.get_vector("left","right","up","down")
+	if(Input.is_action_just_pressed("tool_forward") or Input.is_action_just_pressed("tool_backward")):
+		var tool_direction = Input.get_axis("tool_backward","tool_forward") as int
+		current_tool = posmod(current_tool + tool_direction, Tools.size()) as Tools
 
 func animation():
 	if direction:
